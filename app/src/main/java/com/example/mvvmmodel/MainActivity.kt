1b0a756c14799226983e2fb8f6c729a0df95554b
aleena6875 <aleenashibu48@gmail.com>
2023-08-30 14:17:32 +0530

    Created main activity_main.xml and its kotlin file.

--- app/src/main/java/com/example/mvvmmodel/MainActivity.kt
+++ app/src/main/java/com/example/mvvmmodel/MainActivity.kt
@@ -2,26 +2,26 @@ package com.example.mvvmmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewmodel:ItemViewModel
+    lateinit var viewmodel: ItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewmodel=ViewModelProvider(this).get(ItemViewModel::class.java)
-        var r1=findViewById<RecyclerView>(R.id.r1)
-        val layoutManager=LinearLayoutManager(this)
-        r1.layoutManager=layoutManager
+        viewmodel = ViewModelProvider(this).get(ItemViewModel::class.java)
+        var r1 = findViewById<RecyclerView>(R.id.r1)
+        val layoutManager = LinearLayoutManager(this)
+        r1.layoutManager = layoutManager
         viewmodel.getData()
-        viewmodel.response.observe(this){
-            result->
-            var adapter=ItemAdapter(result)
-            r1.adapter=adapter
+        viewmodel.response.observe(this) { result ->
+            var adapter = ItemAdapter(result)
+            r1.adapter = adapter
         }
-
 
     }
 }
